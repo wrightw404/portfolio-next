4c811fce502f21c004d6a59487fe24f368a507fc
Will <wrightw404@gmail.com>
2022-12-01 16:02:51 -0500

    navbar and logos

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
-    <div></div>
+    <div className='fixed w-full h-20 shadwo-xl z-[100]'>
+        <div className='flex justify-between items-center w-full h-full px-2 2-xl:px-16'> 
+        <Image src='../public/assets/logos/cLogo.png' alt='logo' />
+        </div>
+    </div>
 }
 
 export default Navbar;
