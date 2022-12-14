57c8a0bf6baf892fea46326d8887dda9bec4acd3
Will <wrightw404@gmail.com>
2022-12-14 17:47:22 -0500

    navbar work

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Navbar = () => {
+export default function Navbar() {
     <div className='fixed w-full h-20 shadwo-xl z-[100]'>
         <div className='flex justify-between items-center w-full h-full px-2 2-xl:px-16'> 
-        <Image src='../public/assets/logos/cLogo.png' alt='logo' />
+        <Image src='/../public/assets/logos/cLogo.png' alt='logo' width='125' height='50' />
         </div>
     </div>
 }
 
-export default Navbar;
