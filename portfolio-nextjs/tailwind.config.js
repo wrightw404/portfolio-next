439776ceccfeed45430429f9479225638cbb66d0
Will <wrightw404@gmail.com>
2022-12-14 22:51:00 -0500

    added navbar

--- portfolio-nextjs/tailwind.config.js
+++ portfolio-nextjs/tailwind.config.js
@@ -8,10 +8,14 @@ module.exports = {
     extend: {
       backgroundImage: {
         'cove-bg-image': "url('../public/mainBackground.jpg')",
+        'nav-logo': "url('../public/assets/logos/cLogo.png')",
       },
       colors: {
         'electric-blue': '#66FCF1',
-        'cool-grey': '#C5C6C7'
+        'cool-grey': '#C5C6C7',
+        'cool-white': '#ECF0F3',
+        'dark-blue': '#45A29E',
+
       },
     },
   },
